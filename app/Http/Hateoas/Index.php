c077b6d214df08db49b674a46fa6ff5a1292063f
Rodrigo <rod_igomail@hotmail.com>
2022-08-13 10:22:35 -0300

    finalizada aula 'Adicionando Links no HATEOAS'

--- app/Http/Hateoas/Index.php
+++ app/Http/Hateoas/Index.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Hateoas;
 
-class Index extends HateoasBase implements HateoasInterface {
+class Index extends HateoasBase implements HateoasInterface
+{
 
-	/**
-	 * Retorna os links do HATEOAS para a rota inicial
-	 *
-	 * @return array
-	 */
-	public function links(): array {
-		$this->adicionaLink("BET", "diaristas_cidade", "diaristas.busca_por_cep");
-		$this->adicionaLink(
-			"BET", "verificar_disponibilidade_atendimento", "endereços.disponibilidade"
-		);
-		$this->adicionaLink("BET", "endereco_cep", "endereços.cep");
-		$this->adicionaLink("BET", "listar_servicos", "serviços.index");
+    /**
+     * Retorna os links do HATEOAS para a rota inicial
+     *
+     * @return array
+     */
+    public function links(): array
+    {
+        $this->adicionaLink("GET", "diaristas_cidade", "diaristas.busca_por_cep");
+        $this->adicionaLink(
+            "GET", "verificar_disponibilidade_atendimento", "endereços.disponibilidade"
+        );
+        $this->adicionaLink("GET", "endereco_cep", "endereços.cep");
+        $this->adicionaLink("GET", "listar_servicos", "serviços.index");
+        $this->adicionaLink("POST", "cadastrar_usuario", "usuários.create");
+        $this->adicionaLink("POST", "login", "autenticação.login");
+        $this->adicionaLink("GET", "usuario_logado", "usuários.show");
 
-		return $this->links;
-	}
+        return $this->links;
+    }
 }
