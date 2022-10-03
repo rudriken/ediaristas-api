cc0db7c7b7814da07ddd2551a25bbd60be9c09b8
Rodrigo <rod_igomail@hotmail.com>
2022-10-03 11:34:40 -0300

    finalizada aula 'Adicionando Link da Lista de Diárias ao HATEOAS'

--- app/Http/Hateoas/Usuario.php
+++ app/Http/Hateoas/Usuario.php
@@ -15,9 +15,21 @@ class Usuario extends HateoasBase implements HateoasInterface
      */
     public function links(?Model $usuario): array
     {
+        $this->adicionaLink("GET", "lista_diarias", "diarias.index");
+        $this->linksDoCliente($usuario);
+        return $this->links;
+    }
+
+    /**
+     * Adiciona os links específicos do usuário do tipo 'cliente'
+     *
+     * @param Model $usuario
+     * @return void
+     */
+    private function linksDoCliente(Model $usuario): void
+    {
         if ($usuario->tipo_usuario === 1) {
             $this->adicionaLink("POST", "cadastrar_diaria", "diarias.store");
         }
-        return $this->links;
     }
 }
