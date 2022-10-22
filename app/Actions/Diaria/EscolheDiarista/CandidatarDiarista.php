b8aa4811de6b62da7e58a24de916f3d73f244fd3
Rodrigo <rod_igomail@hotmail.com>
2022-10-22 10:18:32 -0300

    finalizada aula 'Tipo de Usuário e Duplicidade'

--- app/Actions/Diaria/EscolheDiarista/CandidatarDiarista.php
+++ app/Actions/Diaria/EscolheDiarista/CandidatarDiarista.php
@@ -2,9 +2,12 @@
 
 namespace App\Actions\Diaria\EscolheDiarista;
 
+use App\Models\CandidatosDiaria;
 use App\Models\Diaria;
 use App\Verificadores\Diaria\ValidaStatusDiaria;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\ValidationException;
 
 class CandidatarDiarista
 {
@@ -14,8 +17,23 @@ class CandidatarDiarista
 
     public function executar(Diaria $diaria)
     {
+        Gate::authorize("tipo-diarista");
         $this->validaStatusDiaria->executar($diaria, 2);
+        $this->verificarDuplicidadeDeCandidato($diaria);
         $diaria->candidatos()->create(["diarista_id" => Auth::user()->id]);
         dd("depois da gravação");
+    }
+
+    private function verificarDuplicidadeDeCandidato(Diaria $diaria)
+    {
+        $diaristaCandidato = $diaria
+            ->candidatos()
+            ->where("diarista_id", Auth::user()->id)
+            ->first();
+        if ($diaristaCandidato) {
+            throw ValidationException::withMessages([
+                "dado_criacao" => "O(A) diarista já é candidato(a) dessa diária"
+            ]);
+        }
     }
 }
