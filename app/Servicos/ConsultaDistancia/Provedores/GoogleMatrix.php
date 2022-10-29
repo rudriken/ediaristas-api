f0634039a7fb6e12a3c9d63ae23e7e61b426c446
Rodrigo <rod_igomail@hotmail.com>
2022-10-29 10:52:39 -0300

    finalizada aula 'Validando e Formatando CEP'

--- app/Servicos/ConsultaDistancia/Provedores/GoogleMatrix.php
+++ app/Servicos/ConsultaDistancia/Provedores/GoogleMatrix.php
@@ -12,8 +12,37 @@ class GoogleMatrix
         $licenca = new StandardLicense(config("google.key"));
 
         return DistanceMatrix::license($licenca)
-            ->addOrigin($origem)
-            ->addDestination($destino)
+            ->addOrigin($this->formataCEP($origem))
+            ->addDestination($this->formataCEP($destino))
             ->request();
+    }
+
+    /**
+     * Valida e formata o CEP
+     *
+     * @param string $cep
+     * @return string
+     */
+    private function formataCEP(string $cep): string
+    {
+        $this->verificaPadraoCEP($cep);
+        return substr_replace($cep, "-", 5, 0);
+    }
+
+    /**
+     * Verifica o tamanho e padrão do CEP
+     *
+     * @param string $cep
+     * @return void
+     */
+    private function verificaPadraoCEP(string $cep): void
+    {
+        if (strlen($cep) !== 8) {
+            throw new \Exception("O CEP deve ter 8 dígitos", 1);
+        }
+
+        if (!preg_match("/^[0-9]+$/", $cep)) {
+            throw new \Exception("O CEP deve ter apenas números", 1);
+        }
     }
 }
