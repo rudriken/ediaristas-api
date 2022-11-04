5aa8834c31625bee959f070a0e244ee3c4b0778f
Rodrigo <rod_igomail@hotmail.com>
2022-11-04 12:24:57 -0300

    finalizada aula 'Escolhendo o Melhor Diarista para Cada Diária'

--- app/Actions/Diaria/EscolheDiarista/SelecionaAutomaticamente.php
+++ app/Actions/Diaria/EscolheDiarista/SelecionaAutomaticamente.php
@@ -3,17 +3,32 @@
 namespace App\Actions\Diaria\EscolheDiarista;
 
 use App\Models\Diaria;
+use App\Tarefas\Diarista\SelecionaDiaristaIndice;
 use Carbon\Carbon;
 
 class SelecionaAutomaticamente
 {
+
+    public function __construct(private SelecionaDiaristaIndice $selecionaDiaristaIndice)
+    {
+    }
+
     public function executar()
     {
         $diarias = Diaria::where("status", 2)
             ->where("created_at", "<", Carbon::now()->subHours(24))
+            ->withCount("candidatos")
             ->get();
         foreach ($diarias as $diaria) {
-            var_dump($diaria);
+            if ($diaria->candidatos_count === 1) {
+                $diaria->confirmarDiaria($diaria->candidatos[0]->diarista_id);
+                continue;
+            }
+            if ($diaria->candidatos_count > 1) {
+                $diaria->confirmarDiaria(
+                    $this->selecionaDiaristaIndice->executar($diaria)
+                );
+            }
         }
     }
 }
--- teste.sql
+++ teste.sql
@@ -1,2 +1,4 @@
-DELETE FROM ediaristas_temp.candidatos_diaria WHERE (id = '6');
-SELECT * FROM ediaristas_temp.candidatos_diaria;
+SELECT
+    id, data_atendimento, `status`, logradouro, numero, cep, cliente_id, diarista_id,
+    created_at, updated_at
+	FROM ediaristas_temp.diarias;
