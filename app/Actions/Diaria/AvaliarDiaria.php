6756c12b781b0e0e197ac8a693950bdb5e9184b1
Rodrigo <rod_igomail@hotmail.com>
2022-11-10 10:31:37 -0300

    finalizada aula 'Obtendo o ID do Usuário Avaliado'

--- app/Actions/Diaria/AvaliarDiaria.php
+++ app/Actions/Diaria/AvaliarDiaria.php
@@ -15,7 +15,24 @@ class AvaliarDiaria
     private function criaAvaliacao(Diaria $diaria, array $dadosAvaliacao)
     {
         return $diaria->avaliacoes()->create($dadosAvaliacao + [
-            "visibilidade" => 1, "avaliador_id" => Auth::user()->id, "avaliado_id" => 29,
+            "visibilidade" => 1,
+            "avaliador_id" => Auth::user()->id,
+            "avaliado_id" => $this->obtemUsuarioAvaliadoId($diaria),
         ]);
+    }
+
+    /**
+     * Retorna o ID do usuário que está sendo avaliado
+     *
+     * @param Diaria $diaria
+     * @return integer
+     */
+    private function obtemUsuarioAvaliadoId(Diaria $diaria): int
+    {
+        $tipoUsuarioLogado = Auth::user()->tipo_usuario;
+        if ($tipoUsuarioLogado == 1) {
+            return $diaria->diarista_id;
+        }
+        return $diaria->cliente_id;
     }
 }
