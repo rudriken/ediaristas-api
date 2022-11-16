5499de45c9ecdf5371600a4b5c1b3b66ae3bdf1b
Rodrigo <rod_igomail@hotmail.com>
2022-11-16 18:16:30 -0300

    finalizada aula 'Torna as Avaliações Dinâmicas na Lista de Oportunidades'

--- app/Models/Avaliacao.php
+++ app/Models/Avaliacao.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Avaliacao extends Model
 {
@@ -24,4 +25,14 @@ class Avaliacao extends Model
     protected $fillable = [
         "visibilidade", "nota", "descricao", "diaria_id", "avaliador_id", "avaliado_id",
     ];
+
+    /**
+     * Define a relação com o usuário avaliador
+     *
+     * @return BelongsTo
+     */
+    public function avaliador(): BelongsTo
+    {
+        return $this->belongsTo(User::class, "avaliador_id");
+    }
 }
