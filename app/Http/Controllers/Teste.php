44ec895dce562851e54ba8bf0a185a3521c415f4
Rodrigo <rod_igomail@hotmail.com>
2022-11-21 10:59:02 -0300

    finalizada aula 'Gerando o Card Hash e Finalizando a Transação'

--- app/Http/Controllers/Teste.php
+++ app/Http/Controllers/Teste.php
@@ -18,7 +18,7 @@ class Teste extends Controller
         $pagarme = new Client("ak_test_nE14ZiG433nQG0D3aR0XhpzCj4iPkR");
         $transacao = $pagarme->transactions()->create([
             "amount" => "2000",
-            "card_hash" => "123123",
+            "card_hash" => "5718027_NUgfl4eYznXXW5rjcBIfxKaAmukMZh/rnZwi74SFectJF8ZjELupPbmtW4ZeO5r5yHv93cvTqNiCmrdYlzMFwmBKdsHttu1y6HJu5xkinSufxX+3I4Qw7guarBVlgCn5jPOOw5A5sbw3kYQorzXlfvEWqxteA5ARR8gJ3vRv+TpoqQXkZPMspWma0NdxIpPthbHYCfJtL5e2g4XaT9bVHhyRei5/lXhGM+P6M92jL1EGu0HEUtAbOtUF1orA4Cgh1cgqaYC4pQB24D6NSSPxtJROZ4HBBzQpmopJoql9CzAJx2sBAc0VoRKSUTAl+72dCxZaTh1qObLCVOxZeihy9A==",
             "async" => false,
         ]);
         dd($transacao);
