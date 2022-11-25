c1da4c2a246e6fc9f16067723ff74bedd400284a
Rodrigo <rod_igomail@hotmail.com>
2022-11-25 11:33:38 -0300

    finalizada aula 'Validando Mais de Um Status de Diária'

--- app/Actions/Diaria/Cancelamento/CancelarDiaria.php
+++ app/Actions/Diaria/Cancelamento/CancelarDiaria.php
@@ -3,11 +3,17 @@
 namespace App\Actions\Diaria\Cancelamento;
 
 use App\Models\Diaria;
+use App\Verificadores\Diaria\ValidaStatusDiaria;
 
 class CancelarDiaria
 {
+    public function __construct(private ValidaStatusDiaria $validaStatusDiaria)
+    {
+    }
+
     public function executar(Diaria $diaria)
     {
+        $this->validaStatusDiaria->executar($diaria, [2, 3]);
         dd("cheguei na Action 'CancelarDiaria'", $diaria->getAttributes());
     }
 }
