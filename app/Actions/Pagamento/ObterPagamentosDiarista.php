01472768f560b5fdd8adfdf3b07264cdaca41d37
Rodrigo <rod_igomail@hotmail.com>
2022-12-08 11:57:47 -0300

    finalizada aula 'Buscando Diárias no Banco de Dados'

--- app/Actions/Pagamento/ObterPagamentosDiarista.php
+++ app/Actions/Pagamento/ObterPagamentosDiarista.php
@@ -2,10 +2,18 @@
 
 namespace App\Actions\Pagamento;
 
+use App\Models\Diaria;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+
 class ObterPagamentosDiarista
 {
     public function executar()
     {
-        dd("cheguei no Action 'ObterPagamentosDiarista'.");
+        Gate::authorize("tipo-diarista");
+        $diarista = Auth::user();
+        return Diaria::where("diarista_id", $diarista->id)
+            ->whereIn("status", [4, 6, 7])
+            ->get();
     }
 }
--- app/Http/Controllers/Pagamento/ObtemPagamentosDiarista.php
+++ app/Http/Controllers/Pagamento/ObtemPagamentosDiarista.php
@@ -14,6 +14,7 @@ class ObtemPagamentosDiarista extends Controller
 
     public function __invoke(Request $request)
     {
-        $this->obterPagamentosDiarista->executar();
+        $pagamentos = $this->obterPagamentosDiarista->executar();
+        return $pagamentos;
     }
 }
